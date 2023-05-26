fe6f3f3851f4c9a2063928e86928023af599ade3
PREETI2096 <preetirg96@gmail.com>
2023-05-26 12:54:43 +0530

    logical programs

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            bool isPrime = true;
+            Console.Write("Enter the Number to check Prime: ");
+            n = int.Parse(Console.ReadLine());
+           
+            for (int i = 2; i <n/2 ; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("Number is not Prime.");
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                Console.Write("Number is Prime.");
+        }
+    }
+}
